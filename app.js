fba85ac0a40f077ef752eafb2980dd8690c536a1
cartman82 <cartman82@outlook.com>
2015-10-17 15:46:59 +0200

    sqlite3

---
+++ app.js
@@ -0,0 +1,49 @@
+var Chance = require("chance"),
+	chance = new Chance();
+
+var moduleName = process.argv[2];
+if (!moduleName) {
+	console.log("Usage: ", process.argv[0], process.argv[1], "<sqlite/file>");
+	process.exit(0);
+}
+
+var db = require("./" + moduleName);
+
+console.log("Initializing data...");
+
+var data = {},
+	ids = [],
+	keys = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+for (var i = 0; i < 1000; i++) {
+	var item = {
+		id: chance.guid()
+	};
+	for (var j = 0; j < keys.length; j++) {
+		item[keys[j]] = chance.string();
+	}
+	data[item.id] = item;
+	ids.push(item.id);
+}
+
+console.log("Initializing database...");
+
+db.init(function () {
+	console.log("Starting main loop...");
+	
+	var mainLoop = setInterval(function () {
+		// Update data item
+		var id = chance.pick(ids);
+		var key = chance.pick(keys);
+		data[id][key] = chance.string();
+
+		db.set("00001", id, data[id]);
+		console.log(id + "[" + key + "] = " + data[id][key]);
+	}, 1000);
+
+	process.on("SIGINT", function() {
+		db.dispose();
+		clearInterval(mainLoop)
+		console.log("Done");
+	});
+});
---
+++ sqlite.js
@@ -0,0 +1,33 @@
+
+var sqlite3 = require("sqlite3").verbose();
+
+var db = new sqlite3.Database("/tmp/sqlite.db");
+
+exports.init = function (callback) {
+	db.serialize(function() {
+		db.run("DROP TABLE IF EXISTS Tasks_0001");
+		db.run("CREATE TABLE Tasks_0001 (uuid VARCHAR PRIMARY KEY, data TEXT)", function (err) {
+			return callback(err);
+		});
+	});
+}
+
+exports.set = function (collection, id, data, callback) {
+	data = JSON.stringify(data);
+	db.run("INSERT OR REPLACE INTO Tasks_0001 VALUES($1, $2)", [id, data], function (err) {
+		if (err) {
+			if (callback) {
+				callback(err);
+			} else {
+				console.error(err);
+			}
+			return;
+		}
+
+		callback && callback(null);
+	});
+};
+
+exports.dispose = function () {
+	db.close();
+};
